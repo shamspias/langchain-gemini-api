e6967c26520812f8d2c1787fcfb90eb99a21013b
shamspias <shamsuddinpias0@gmail.com>
2023-12-19 15:34:44 +0600

    fix config with system INSTRUCTION

--- app/config.py
+++ app/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     MY_API_KEY: str
     GEMINI_API_KEY: str
     REDIS_URL: str = "redis://localhost:6379/5"
+    SYSTEM_INSTRUCTION: str = "Please describe how the ai should respond to the user."
 
     class Config:
         env_file = ".env"
