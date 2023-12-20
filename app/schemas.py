d1ec35ce61b16144a87552c6fd661528dedbb6d6
shamspias <shamsuddinpias0@gmail.com>
2023-12-20 12:26:22 +0600

    fix: conversation id

--- app/schemas.py
+++ app/schemas.py
@@ -5,7 +5,6 @@ from typing import Optional, Text
 class ChatRequest(BaseModel):
     message_id: Optional[str] = None
     query: str
-    conversation_id: Optional[str]
     image: Optional[bool] = False
     image_url: Optional[str] = None
 
