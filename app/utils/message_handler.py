68d8e0ccba7f78610cd5c4784887940b2c129d1b
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2023-12-26 08:40:26 +0600

    del cache manager from handler

--- app/utils/message_handler.py
+++ app/utils/message_handler.py
@@ -1,16 +1,15 @@
 import logging
 
-from typing import AsyncIterable, Dict
+from typing import AsyncIterable
 
 from .llm_manager import GeminiLLMManager
-from .cache_manager import CacheManager
 
 logger = logging.getLogger(__name__)
 
 
 class MessageHandler:
-    def __init__(self, cache_manager: CacheManager):
-        self.cache_manager = cache_manager
+    def __init__(self, llm_manager: GeminiLLMManager):
+        self.llm_manager = llm_manager
 
     async def send_message_async(
             self, message: str,
@@ -19,11 +18,7 @@ class MessageHandler:
             image_url: str = None
     ) -> AsyncIterable[str]:
         try:
-
-            # LLM configuration
-            llm_manager = GeminiLLMManager()
-
-            async for token in llm_manager.generate_async_response(message, conversation_id, image, image_url):
+            async for token in self.llm_manager.generate_async_response(message, conversation_id, image, image_url):
                 yield token
         except Exception as e:
             logger.error(f"Error in send_message: {e}")
@@ -31,10 +26,5 @@ class MessageHandler:
 
     async def flush_conversation_cache(self, project_id: str):
         # LLM configuration
-        llm_manager = GeminiLLMManager()
-        history = llm_manager.create_or_get_memory(project_id)
+        history = self.llm_manager.create_or_get_memory(project_id)
         history.clear()
-        await self.cache_manager.flush_conversation_cache(project_id)
-
-    async def save_conversation_config(self, project_id: str, config: Dict):
-        await self.cache_manager.save_conversation_config(project_id, config)
